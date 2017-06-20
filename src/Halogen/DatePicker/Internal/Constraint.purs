07600abf93835f20a711da516e48e92469b55e7f
Irakli Safareli <i.safareli@gmail.com>
2017-06-20 20:45:47 +0400

    use when/unless

--- src/Halogen/DatePicker/Internal/Constraint.purs
+++ src/Halogen/DatePicker/Internal/Constraint.purs
@@ -1,11 +1,11 @@
 module Halogen.Datapicker.Internal.Constraint where
 
 import Prelude
-import Data.Validation.Semigroup (V, invalid, unV)
+
+import Control.Monad.State (State, get, put, execState)
 import Data.Foldable (class Foldable, any, null, length, for_)
 import Data.List as List
-import Control.Monad.State (State, get, put, execState)
-
+import Data.Validation.Semigroup (V, invalid, unV)
 
 type Constraint a = a -> V (Array String) Unit
 
@@ -13,18 +13,17 @@ runConstraint :: ∀ a g. Constraint (g a) -> g a -> Array String
 runConstraint f a = unV id (const []) $ f a
 
 allowedValues :: ∀ g a. Foldable g => (a -> String) -> Array (EqPred a) -> Constraint (g a)
-allowedValues showVal as as' = for_ as' \a ->
-  if matchesAny a as
-  then pure unit
-  else invalid ["Contains value: " <> (showVal a) <> " which is not allowed"]
+allowedValues showVal as as' = for_ as' \a -> unless
+  (matchesAny a as) 
+  (invalid ["Contains value: " <> (showVal a) <> " which is not allowed"])
 
 notEmpty :: ∀ f a. Foldable f => Constraint (f a)
-notEmpty as = if null as then invalid ["Input must contain values"] else pure unit
+notEmpty as = when (null as) (invalid ["Input must contain values"])
 
 data Sorting = Increasing | Decreasing
 
 sorted :: ∀ f a. Ord a => Foldable f => Sorting -> Constraint (f a)
-sorted sorting as = if isSorted then pure unit else invalid ["Input Must be sorted"]
+sorted sorting as = unless isSorted (invalid ["Input Must be sorted"])
   where
   isSorted = case sorting of
     Increasing -> List.reverse asList == List.sort asList
@@ -35,14 +34,12 @@ sorted sorting as = if isSorted then pure unit else invalid ["Input Must be sort
 allowNoneOrOne :: ∀ g a. Foldable g => Array (EqPred a) -> Constraint (g a)
 allowNoneOrOne as = f <<< usageCount as
   where
-  f c | c > 1 = invalid ["Usage count (" <> show c <> ") for allowed elements (" <> (show as) <> ") must be 0 or 1"]
-  f _ = pure unit
+  f c = when (c > 1) $ invalid ["Usage count (" <> show c <> ") for allowed elements (" <> (show as) <> ") must be 0 or 1"]
 
 allowNoneOrAll :: ∀ f a. Foldable f => Array (EqPred a) -> Constraint (f a)
 allowNoneOrAll as = f <<< usageCount as
   where
-  f c | c /= 0 && c /= length as = invalid ["Usage count (" <> show c <> ") for allowed elements (" <> (show as) <> ") must be 0 or all"]
-  f _ = pure unit
+  f c  = when (c /= 0 && c /= length as) $ invalid ["Usage count (" <> show c <> ") for allowed elements (" <> (show as) <> ") must be 0 or all"]
 
 matchesAny :: ∀ a . a -> Array (EqPred a) -> Boolean
 matchesAny = any <<< equals
@@ -51,7 +48,7 @@ usageCount :: ∀ f a. Foldable f => Array (EqPred a) -> f a -> Int
 usageCount as as' = for_ as' incState `execState` 0
   where
   incState :: a -> State Int Unit
-  incState a = get >>= \count -> if matchesAny a as then put (count + 1) else pure unit
+  incState a = get >>= \count -> when (matchesAny a as) (put $ count + 1)
 
 
 data EqPred a = EqPred String (a -> Boolean)
